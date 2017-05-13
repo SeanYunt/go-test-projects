8f0ad7e92fa5e07ace58719a51644902e928fee2
kirk <synchrophoto@gmail.com>
2017-05-12 17:43:58 -0700

    First commit

---
+++ go-hid-card-reader/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"net/http"
+	"os"
+)
+
+type Config struct {
+	DataFileLoc string `json:"datafileloc"`
+}
+
+type WorkRecord struct { //define the structure for the work hour.
+	MemberCardNumber string
+	WorkType         string
+	WorkTypeOther    string
+	HoursWorked      string
+	PictureLoc       string
+	/* I need to figure out how to convert date time for json. this is a little more involved.
+	DTS              time.Time */
+}
+
+func LoadConfiguration(filename string) (Config, error) {
+	var config Config
+	configFile, err := os.Open(filename)
+	defer configFile.Close()
+	if err != nil {
+		return config, err
+	}
+	jsonParser := json.NewDecoder(configFile)
+	err = jsonParser.Decode(&config)
+	return config, err
+}
+
+func open(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("index.html")
+	t.Execute(w, nil)
+}
+
+func addhours(w http.ResponseWriter, r *http.Request) {
+	config, _ := LoadConfiguration("config.json")
+	f, err := os.OpenFile(config.DataFileLoc, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	defer f.Close()
+
+	wr := new(WorkRecord)
+	wr.MemberCardNumber = r.FormValue("membercardnumber")
+	wr.WorkType = r.FormValue("worktype")
+	wr.HoursWorked = r.FormValue("hoursworked")
+
+	b, err := json.Marshal(wr)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	f.Write(b)
+	f.Close()
+}
+
+func main() {
+	fmt.Println("Starting the Application...")
+	config, _ := LoadConfiguration("config.json")
+	fmt.Println(config.DataFileLoc)
+	http.HandleFunc("/addhours", addhours)
+	http.HandleFunc("/", open)
+	http.ListenAndServe(":8080", nil)
+}
